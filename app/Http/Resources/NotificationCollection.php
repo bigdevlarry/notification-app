099336b8ce01b5d456cd309b6ad85fda33a22bdd
larry.sule-balogun <larry.sule-balogun@totara.com>
2026-05-04 20:19:01 +0100

    update notification collection to use simple pagination response format

--- app/Http/Resources/NotificationCollection.php
+++ app/Http/Resources/NotificationCollection.php
@@ -10,7 +10,24 @@ class NotificationCollection extends ResourceCollection
     public function toArray(Request $request): array
     {
         return [
-            'data' => $this->collection,
+            'data'  => $this->collection,
+            'meta'  => [
+                'current_page' => $this->resource->currentPage(),
+                'last_page'    => $this->resource->lastPage(),
+                'per_page'     => $this->resource->perPage(),
+                'total'        => $this->resource->total(),
+            ],
+            'links' => [
+                'first' => $this->resource->url(1),
+                'last'  => $this->resource->url($this->resource->lastPage()),
+                'prev'  => $this->resource->previousPageUrl(),
+                'next'  => $this->resource->nextPageUrl(),
+            ],
         ];
+    }
+
+    public function paginationInformation(mixed $request, array $paginated, array $default): array
+    {
+        return [];
     }
 }
